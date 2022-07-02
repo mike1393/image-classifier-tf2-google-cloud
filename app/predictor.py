8882ca190c9f1d38d51a3b61a13c8afb1789273e
mike1393 <mike.chen1393@gmail.com>
2022-07-02 19:32:00 +0800

    Removing model from app
    The app now gets prediction from ai platform instead of getting it in-script.
    THis makes the app light weight and model easy to manage.

--- app/predictor.py
+++ app/predictor.py
@@ -2,20 +2,24 @@
 from flask import Flask, request, redirect, render_template
 from werkzeug.utils import secure_filename
 import numpy as np
-import tensorflow as tf
 #Built-in
 import os
 #Local
+from utils import parse_function, predict_json
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/mike/project/image_classifier_tf2_google_cloud/image-classifier-tf2-google-cloud/trainer/Credentials/sa-private-key.json" # change for your GCP key
 FOOD_CLASSES = [
     "Bread", "Dairy_product", "Dessert", "Egg", "Fried_food", 
     "Meat", "Noodles_or_Pasta", "Rice", "Seafood", "Soup", "Vegetable_or_Fruit"]
+PROJECT = "cloud-image-classifier-tf2"
+REGION = "asia-east1"
+MODEL = "kaggle_food_11_savedmodel"
 
 app = Flask(__name__, static_url_path="/static")
 app.config["IMAGE_UPLOADS"] = "./static"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = [".JPEG", ".JPG", ".PNG"]
-food_prediction_model = tf.keras.models.load_model("./savedmodel")
+
 
 
 def is_allowed_file(filename):
@@ -29,23 +33,31 @@ def is_allowed_file(filename):
     print(f"[ERROR] File extension:{ext} NOT allowed.")
     return False
 
-def parse_function(filepath):
-    img_string = tf.io.read_file(filepath)
-    img = tf.image.decode_image(img_string,channels=3)
-    img = tf.image.convert_image_dtype(img,tf.float32)
-    img = tf.image.resize(img,[299,299])
-    img = tf.expand_dims(img, axis=0) 
-    return img
-
-def get_prediction(image):
+def parse_prediction(probability_list):
     predicted_result = {}
-    probability_list = food_prediction_model.predict(image)
     predicted_result["result"] = FOOD_CLASSES[np.argmax(probability_list)]
     probability_list = list(np.round(probability_list*100, 2))[0]
     predicted_result["probabilities"] = dict(zip(FOOD_CLASSES, probability_list))
     predicted_result["result"] = FOOD_CLASSES[np.argmax(probability_list)]
     return predicted_result
 
+def make_prediction(image_file, project, region, model):
+    """
+    Takes an image and uses model (a trained TensorFlow model) to make a
+    prediction.
+    Returns:
+     image (preproccessed)
+     pred_class (prediction class from class_names)
+     pred_conf (model confidence)
+    """
+    image = parse_function(image_file)
+    
+    # image = tf.expand_dims(image, axis=0)
+    preds = predict_json(project=project,
+                         region=region,
+                         model=model,
+                         instances=image)
+    return parse_prediction(preds)
 
 @app.route("/",methods=["GET","POST"])
 def upload_image():
@@ -66,8 +78,7 @@ def upload_image():
 def showing_image(image_name):
     if request.method == "POST":
         image_path = os.path.join(app.config["IMAGE_UPLOADS"], image_name)
-        image = parse_function(image_path)
-        predicted_result = get_prediction(image)
+        predicted_result = make_prediction(image_path,PROJECT,REGION,MODEL)
         print(f"[DEBUG] predicted_result: {predicted_result}")
         return render_template(
             "prediction_result.html",
